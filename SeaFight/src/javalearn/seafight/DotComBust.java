339be32062993dc13bfa9f8815bc8736c25ea604
Андрей Парфенов <58725265+Andr1981@users.noreply.github.com>
2020-04-20 16:37:04 +0300

    Доделал игру

---
+++ SeaFight/src/javalearn/seafight/DotComBust.java
@@ -0,0 +1,66 @@
+package javalearn.seafight;
+
+import java.util.ArrayList;
+
+
+public class DotComBust {
+    private GameHelper helper = new GameHelper();
+    private ArrayList<DotCom> dotComsList = new ArrayList<DotCom>();
+    private int numOfGuesses = 0;
+
+    public void setUpGame() {
+        DotCom one = new DotCom();
+        one.setName("Pets.com");
+        DotCom two = new DotCom();
+        two.setName("eToys.com");
+        DotCom three = new DotCom();
+        three.setName("Go2.com");
+        dotComsList.add(one);
+        dotComsList.add(two);
+        dotComsList.add(three);
+
+        System.out.println("Ваша цель - потопить три сайта");
+        System.out.printf("%s %s %s%n", one.getName(), two.getName(), three.getName());
+        System.out.println("Попытайтесь потопить их за минимальное количество ходов");
+
+        for (DotCom dotComToSet : dotComsList) {
+            ArrayList<String> newLocation = helper.placeDotCom(3);
+            dotComToSet.setLocationCells(newLocation);
+        }
+    }
+
+    public void startPlaying() {
+        while (!dotComsList.isEmpty()) {
+            String userGuess = helper.getUserInput("Ваш выстрел ");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+    }
+
+    private void checkUserGuess(String userGuess) {
+        numOfGuesses++;
+        String result = "Мимо";
+        for (DotCom dotComToTest : dotComsList) {
+            result = dotComToTest.checkYourself(userGuess);
+            if (result.equals("Попал")) {
+                break;
+            }
+            if (result.equals("Потопил")) {
+                dotComsList.remove(dotComToTest);
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private void finishGame() {
+        System.out.println("Все сайты ушли ко дну!");
+        if (numOfGuesses < 18) {
+            System.out.println("Молодец! Количество попыток - " + numOfGuesses);
+        }else {
+            System.out.println("Могло бы быть лучше! Количество попыток - " + numOfGuesses);
+        }
+    }
+
+
+}
---
+++ SeaFight/src/javalearn/seafight/SeaFifhtGame.java
@@ -0,0 +1,10 @@
+package javalearn.seafight;
+
+public class SeaFifhtGame {
+
+    public static void main(String[] args) {
+        DotComBust game = new DotComBust();
+        game.setUpGame();
+        game.startPlaying();
+    }
+}
